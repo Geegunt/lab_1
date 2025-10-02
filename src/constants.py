f420a5b3ebdb4b8da9b2854964b18881dd71ec80
Gleb Grigorev <gagrigorev@mai.ru>
2025-10-02 09:14:17 +0300

    commit

--- src/constants.py
+++ src/constants.py
@@ -1,1 +1,14 @@
-SAMPLE_CONSTANT: int = 10
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def is_empty(self):
+        return self.items == []
+
+
